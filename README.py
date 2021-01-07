472517dbd7d04df4f144a8748853f40f0d4de7bb
lopes7890 <72554824+lopes7890@users.noreply.github.com>
2021-01-07 11:27:35 -0300

    Update and rename README.md to README.py
    Jogo simples de pedra,papel,tesoura.
    (Programa para mim aprender a usar o Github)

---
+++ README.py
@@ -0,0 +1,10 @@
+# jornadabylearn-
+import random
+valor = random.randint(1, 3)
+p = input("pedra,papel ou tesoura? ")
+if valor == 1:
+    print("pedra")
+if valor == 2:
+    print("papel")
+if valor == 3:
+    print("tesoura")   
